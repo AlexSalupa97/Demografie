658ce2e12cf99157f79f0c384f57951f23673bdf
alexsalupa97 <alexsalupa97@gmail.com>
2019-02-05 01:53:44 +0200

    Aplicatie v2

--- app/src/main/java/com/example/demografie/Activitati/Activitati/InformatiiActivity.java
+++ app/src/main/java/com/example/demografie/Activitati/Activitati/InformatiiActivity.java
@@ -1,16 +1,31 @@
 package com.example.demografie.Activitati.Activitati;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.demografie.R;
 
 public class InformatiiActivity extends AppCompatActivity {
 
+    Button btnImag;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informatii);
+
+        btnImag=(Button)findViewById(R.id.btnImag);
+
+        btnImag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),ImagineActivity.class);
+                startActivity(intent);
+            }
+        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
